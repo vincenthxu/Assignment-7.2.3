6fe0b96cf5c9e60c1547ac1dc641902db0f37e64
Vincent Xu <vincent.xu1@outlook.com>
2025-08-19 13:28:11 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment_7._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnagram("rat", "car"));
+            Console.WriteLine(IsAnagram("anagram", "nagaram"));
+        }
+
+        static bool IsAnagram(string s, string t)
+        {
+            char[] a = s.ToCharArray();
+            char[] b = t.ToCharArray();
+            Array.Sort(a);
+            Array.Sort(b);
+            return new string(a) == new string(b);
+        }
+    }
+}
